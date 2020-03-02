f469800cbf7ffe55e37d82a2630f519990652cca
Marcin Miler <marcin4951@gmail.com>
2020-03-02 21:16:22 +0100

    [LYAH] Own typeclasses

--- learn-you-a-haskell/Types.hs
+++ learn-you-a-haskell/Types.hs
@@ -159,3 +159,22 @@ sampleTree = foldr treeInsert EmptyTree nums
 contains' = 8 `treeElem` sampleTree
 
 -- True
+
+--------------------------------------------------------------------------------
+
+-- Own type classes with instances
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red = True
+    Yellow == Yellow = True
+    Green == Green = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red light"
+    show Yellow = "Yellow light"
+    show Green = "Green light"
+
+----------------------------------------------------------------------------------
